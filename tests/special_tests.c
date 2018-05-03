9cb006ddfe3ee88d85ba7ee3ba05f079717fad1b
Francois Brodeur <francois.brodeur@adgear.com>
2018-05-03 11:24:22 -0400

    Add starts_with tests

--- tests/special_tests.c
+++ tests/special_tests.c
@@ -57,6 +57,49 @@ static int test_contains()
     return 0;
 }
 
+static bool starts_with(bool has_not, const char* attr, bool allow_undefined, const char* pattern, const char* value)
+{
+    struct config* config = make_default_config();
+    add_attr_domain_s(config, attr, allow_undefined);
+    struct ast_node* node = NULL;
+    char* expr;
+    const char* pre;
+    if(has_not) {
+        pre = "not ";
+    }
+    else {
+        pre = "";
+    }
+    asprintf(&expr, "%sstarts_with(%s, \"%s\")", pre, attr, pattern);
+    parse(expr, &node);
+    const char* event_attr = allow_undefined ? "a" : attr;
+    const struct event* event = make_simple_event_s(config, event_attr, value);
+    bool result = match_node(config, event, node);
+    free_config(config);
+    free_ast_node(node);
+    free_event((struct event*)event);
+    return result;
+}
+
+static bool starts_with_a(const char* pattern, const char* value) { return starts_with(false, "a", false, pattern, value); }
+static bool starts_with_b(const char* pattern, const char* value) { return starts_with(false, "b", true, pattern, value); }
+static bool not_starts_with_a(const char* pattern, const char* value) { return starts_with(true, "a", false, pattern, value); }
+
+static int test_starts_with()
+{
+    mu_assert(starts_with_a("", "abc"), "starts_with_in_nil");
+    mu_assert(starts_with_a("abc", "abcc"), "starts_with_in_start");
+    mu_assert(starts_with_a("abc", "abc"), "starts_with_in_exact");
+    mu_assert(!starts_with_a("abc", "a"), "starts_with_out_var_smaller");
+    mu_assert(!starts_with_a("abc", ""), "starts_with_out_var_nil");
+    mu_assert(!starts_with_b("abc", "abc"), "starts_with_out_undef");
+    // mu_assert(!not_starts_with_a("abc", "abcc"), "starts_with_not_start");
+    // mu_assert(!not_starts_with_a("abc", "abc"), "starts_with_not_exact");
+    // mu_assert(not_starts_with_a("abc", "a"), "starts_with_not_var_smaller");
+    // mu_assert(not_starts_with_a("abc", "", "starts_with_not_var_nil");
+    return 0;
+}
+
 int all_tests() 
 {
     // mu_run_test(test_within_frequency_cap);
@@ -64,7 +107,7 @@ int all_tests()
     // mu_run_test(test_segment_before);
     // mu_run_test(test_geo);
     mu_run_test(test_contains);
-    // mu_run_test(test_starts_with);
+    mu_run_test(test_starts_with);
     // mu_run_test(test_ends_with);
 
     return 0;
