26e6c35470066aeeb92b6b516036e4b354d629ef
Francois Brodeur <francois.brodeur@adgear.com>
2018-05-11 11:47:19 -0400

    Format utils + change behavior for newline

--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,12 @@
 #include "utils.h"
 
 #include <math.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
 
 int64_t random_in_range(int64_t min, int64_t max)
 {
@@ -18,7 +18,7 @@ bool random_bool()
     return random_in_range(false, true);
 }
 
-bool feq(double a, double b) 
+bool feq(double a, double b)
 {
     return fabs(a - b) < __DBL_EPSILON__;
 }
@@ -28,7 +28,7 @@ bool fne(double a, double b)
     return !feq(a, b);
 }
 
-void switch_default_error(const char* str) 
+void switch_default_error(const char* str)
 {
     fprintf(stderr, "%s", str);
     abort();
--- src/utils.h
+++ src/utils.h
@@ -1,18 +1,22 @@
 #pragma once
 
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdarg.h>
 
-#define max(a,b) \
-    ({ __typeof__ (a) _a = (a); \
-        __typeof__ (b) _b = (b); \
-        _a > _b ? _a : _b; })
+#define max(a, b)               \
+    ({                          \
+        __typeof__(a) _a = (a); \
+        __typeof__(b) _b = (b); \
+        _a > _b ? _a : _b;      \
+    })
 
-#define min(a,b) \
-    ({ __typeof__ (a) _a = (a); \
-        __typeof__ (b) _b = (b); \
-        _a < _b ? _a : _b; })
+#define min(a, b)               \
+    ({                          \
+        __typeof__(a) _a = (a); \
+        __typeof__(b) _b = (b); \
+        _a < _b ? _a : _b;      \
+    })
 
 int64_t random_in_range(int64_t min, int64_t max);
 bool random_bool();
@@ -20,7 +24,7 @@ bool random_bool();
 bool feq(double a, double b);
 bool fne(double a, double b);
 void switch_default_error(const char* str);
-//char* strdup(const char* str);
+// char* strdup(const char* str);
 int asprintf(char** buf, const char* format, ...);
 int vasprintf(char** buf, const char* format, va_list va);
 
