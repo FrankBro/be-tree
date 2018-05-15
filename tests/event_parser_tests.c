058bbeddf2fbd587abe4e1193c2eef174f040ae3
Francois Brodeur <francois.brodeur@adgear.com>
2018-05-15 16:33:32 -0400

    integer list tests

--- tests/event_parser_tests.c
+++ tests/event_parser_tests.c
@@ -36,6 +36,45 @@ bool test_string_pred(const char* attr, const char* value, const struct event* e
         && strcmp(pred->value.svalue.string, value) == 0;
 }
 
+bool test_integer_list_pred(
+    const char* attr, struct integer_list_value list, const struct event* event, size_t index)
+{
+    const struct pred* pred = event->preds[index];
+    if(strcmp(pred->attr_var.attr, attr) == 0 && pred->value.value_type == VALUE_IL) {
+        if(list.count == pred->value.ilvalue.count) {
+            for(size_t i = 0; i < list.count; i++) {
+                if(list.integers[i] != pred->value.ilvalue.integers[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+bool test_integer_list_pred0(const char* attr, const struct event* event, size_t index)
+{
+    struct integer_list_value list = { .count = 0, .integers = NULL };
+    return test_integer_list_pred(attr, list, event, index);
+}
+
+bool test_integer_list_pred1(const char* attr, int64_t i1, const struct event* event, size_t index)
+{
+    struct integer_list_value list = { .count = 0, .integers = NULL };
+    add_integer_list_value(i1, &list);
+    return test_integer_list_pred(attr, list, event, index);
+}
+
+bool test_integer_list_pred2(
+    const char* attr, int64_t i1, int64_t i2, const struct event* event, size_t index)
+{
+    struct integer_list_value list = { .count = 0, .integers = NULL };
+    add_integer_list_value(i1, &list);
+    add_integer_list_value(i2, &list);
+    return test_integer_list_pred(attr, list, event, index);
+}
+
 int test_bool()
 {
     struct event* event;
@@ -81,12 +120,28 @@ int test_string()
     return 0;
 }
 
+int test_integer_list()
+{
+    struct event* event;
+    event_parse("{\"single\":[1], \"two\":[1,2], \"empty\":[]}", &event);
+    mu_assert(event->pred_count == 3 && test_integer_list_pred1("single", 1, event, 0)
+            && test_integer_list_pred2("two", 1, 2, event, 1)
+            && test_integer_list_pred0("empty", event, 2),
+        "all cases");
+    free_event(event);
+    return 0;
+}
+
 int all_tests()
 {
     mu_run_test(test_bool);
     mu_run_test(test_integer);
     mu_run_test(test_float);
     mu_run_test(test_string);
+    mu_run_test(test_integer_list);
+    // | string_list_value
+    // | segments_value
+    // | frequencies_value
     return 0;
 }
 
