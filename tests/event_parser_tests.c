6970d784fa332cd6abc8d2d8ed96a0ec7ea99f0a
Francois Brodeur <francois.brodeur@adgear.com>
2018-05-15 16:09:08 -0400

    Refactor + test float

--- tests/event_parser_tests.c
+++ tests/event_parser_tests.c
@@ -8,15 +8,33 @@
 
 int event_parse(const char* text, struct event** event);
 
+bool test_bool_pred(const char* attr, bool value, const struct event* event, size_t index)
+{
+    const struct pred* pred = event->preds[index];
+    return strcmp(pred->attr_var.attr, attr) == 0 && pred->value.value_type == VALUE_B
+        && pred->value.bvalue == value;
+}
+
+bool test_integer_pred(const char* attr, int64_t value, const struct event* event, size_t index)
+{
+    const struct pred* pred = event->preds[index];
+    return strcmp(pred->attr_var.attr, attr) == 0 && pred->value.value_type == VALUE_I
+        && pred->value.ivalue == value;
+}
+
+bool test_float_pred(const char* attr, double value, const struct event* event, size_t index)
+{
+    const struct pred* pred = event->preds[index];
+    return strcmp(pred->attr_var.attr, attr) == 0 && pred->value.value_type == VALUE_F
+        && feq(pred->value.fvalue, value);
+}
+
 int test_bool()
 {
     struct event* event;
     event_parse("{\"true\": true, \"false\": false}", &event);
-    mu_assert(event->pred_count == 2 && strcmp(event->preds[0]->attr_var.attr, "true") == 0
-            && event->preds[0]->value.value_type == VALUE_B && event->preds[0]->value.bvalue == true
-            && strcmp(event->preds[1]->attr_var.attr, "false") == 0
-            && event->preds[1]->value.value_type == VALUE_B
-            && event->preds[1]->value.bvalue == false,
+    mu_assert(event->pred_count == 2 && test_bool_pred("true", true, event, 0)
+            && test_bool_pred("false", false, event, 1),
         "true and false");
     free_event(event);
     return 0;
@@ -26,11 +44,20 @@ int test_integer()
 {
     struct event* event;
     event_parse("{\"positive\": 1, \"negative\": -1}", &event);
-    mu_assert(event->pred_count == 2 && strcmp(event->preds[0]->attr_var.attr, "positive") == 0
-            && event->preds[0]->value.value_type == VALUE_I && event->preds[0]->value.ivalue == 1
-            && strcmp(event->preds[1]->attr_var.attr, "negative") == 0
-            && event->preds[1]->value.value_type == VALUE_I && event->preds[1]->value.ivalue == -1,
+    mu_assert(event->pred_count == 2 && test_integer_pred("positive", 1, event, 0)
+            && test_integer_pred("negative", -1, event, 1),
         "positive and negative");
+    free_event(event);
+    return 0;
+}
+int test_float()
+{
+    struct event* event;
+    event_parse("{\"no decimal\": 1., \"decimal\": 1.2, \"negative\": -1.}", &event);
+    mu_assert(event->pred_count == 3 && test_float_pred("no decimal", 1., event, 0)
+            && test_float_pred("decimal", 1.2, event, 1)
+            && test_float_pred("negative", -1., event, 2),
+        "all cases");
     free_event(event);
     return 0;
 }
@@ -39,6 +66,7 @@ int all_tests()
 {
     mu_run_test(test_bool);
     mu_run_test(test_integer);
+    mu_run_test(test_float);
     return 0;
 }
 
