f3cb7c76257031ab8a6ded8b527dab045ea437c6
Francois Brodeur <francois.brodeur@adgear.com>
2018-12-11 17:21:10 -0500

    rbtree for predmap

--- src/hashmap.h
+++ src/hashmap.h
@@ -2,17 +2,17 @@
 
 #include <stddef.h>
 
-#include "map.h"
+#include "jsw_rbtree.h"
 #include "memoize.h"
 
 struct ast_node;
 
-typedef map_t(struct ast_node*) expr_map_t;
+typedef struct jsw_rbtree exprmap_t;
 
 struct pred_map {
     betree_pred_t pred_count;
     betree_pred_t memoize_count;
-    expr_map_t m;
+    exprmap_t* m;
 };
 
 void assign_pred(struct pred_map* pred_map, struct ast_node* node);
