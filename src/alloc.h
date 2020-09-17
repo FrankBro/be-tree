37b77861169bd0f48079735a4f35bb1af08fc7e5
Francois Brodeur <francois.brodeur.87@gmail.com>
2020-09-17 09:43:58 -0400

    Only use malloc

--- src/alloc.h
+++ src/alloc.h
@@ -1,18 +1,9 @@
 #pragma once
 
-#ifdef NIF
-#include <erl_nif.h>
-#define bmalloc enif_alloc
-#define bcalloc enif_calloc
-#define brealloc enif_realloc
-#define bfree enif_free
-void* enif_calloc(size_t size);
-#else
 #define bmalloc malloc
 #define bcalloc(x) calloc(x, 1)
 #define brealloc realloc
 #define bfree free
-#endif
 
 #include <stdarg.h>
 
